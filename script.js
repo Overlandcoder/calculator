d3b8c06dcc2d9dc59b62e973bce7fa5cc5f078c5
Overlander <anuragnandal@hotmail.com>
2022-11-19 13:29:53 -0500

    Finish project

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ let add = (num1, num2) => num1 + num2;
 let subtract = (num1, num2) => num1 - num2;
 let multiplty = (num1, num2) => num1 * num2;
 let divide = (num1, num2) => num1 / num2;
-let operators = ["+", "-", "x", "%"];
+let operators = ["+", "-", "x", "/"];
 let firstNum;
 let secondNum;
 let operator;
+let currentOperator = [];
 let calculations = 0;
 
 let operate = (num1, num2, operator) => {
@@ -18,61 +19,65 @@ let operate = (num1, num2, operator) => {
       return subtract(num1, num2);
     case "x":
       return multiplty(num1, num2);
-    case "%":
+    case "/":
       return divide(num1, num2);
   }
 }
 
-function resetNumbers() {
-  firstNum = parseInt(screen.textContent);
+function resetValues() {
+  firstNum = parseInt(result.textContent);
   secondNum = undefined;
-  operator = undefined;
-  calculations = 0;
+  operations.textContent = "";
+  currentOperator.shift();
 }
 
 const numberButtons = document.querySelectorAll(".number");
-const screen = document.querySelector(".screen");
+const operatorButtons = document.querySelectorAll(".operator");
+const operations = document.querySelector(".operations");
+const result = document.querySelector(".result");
+const equalButton = document.querySelector(".equal");
+const clearButton = document.querySelector(".btn-clear");
+
 numberButtons.forEach(btn => btn.addEventListener("click", () => {
   let num = btn.textContent;
-  if (screen.textContent == 0) {
-    screen.textContent = num;
+  if (operations.textContent == 0) {
+    operations.textContent = num;
   } else {
-    screen.textContent += num;
+    operations.textContent += num;
   }
 }))
 
-const clearButton = document.querySelector(".btn-clear");
 clearButton.addEventListener("click", () => {
-  screen.textContent = 0;
+  operations.textContent = 0;
+  result.textContent = "";
   firstNum = undefined;
   secondNum = undefined;
-  operator = undefined;
+  currentOperator = [];
   calculations = 0;
 })
 
-const operatorButtons = document.querySelectorAll(".operator");
 operatorButtons.forEach(btn => btn.addEventListener("click", () => {
-  if (!firstNum) {
-    firstNum = parseInt(screen.textContent);
-  } else {
-    secondNum = parseInt(screen.textContent);
-  }
-  console.log(firstNum);
-
   operator = btn.textContent;
-  if (calculations < 1) {
-    screen.textContent = "";
+  currentOperator.push(operator);
+  
+  if (!firstNum) {
+    firstNum = parseInt(operations.textContent);
   } else {
-    screen.textContent = operate(firstNum, secondNum, operator);
-    resetNumbers();
-    console.log(firstNum);
+    secondNum = parseInt(operations.textContent);
   }
-  calculations += 1;
+
+  if (calculations < 1) {
+    operations.textContent = "";
+  } else {
+    result.textContent = operate(firstNum, secondNum, currentOperator[0]);
+    resetValues();
+  }
+  calculations++;
 }))
 
-const equalButton = document.querySelector(".equal");
 equalButton.addEventListener("click", () => {
-  secondNum = parseInt(screen.textContent);
-  screen.textContent = operate(firstNum, secondNum, operator);
-  resetNumbers();
+  secondNum = parseInt(operations.textContent);
+  result.textContent = operate(firstNum, secondNum, currentOperator[0]);
+  resetValues();
+  calculations = 0;
 })
